08cfc5ba74c0b67ffd41721603c37b3ac0df489e
Giovanni Rizzolo <grizzolo@GiovannisLaptop.homenet.telecomitalia.it>
2024-03-24 16:30:09 +0100

    fix: update env variables read from os

--- env/env.go
+++ env/env.go
@@ -1,26 +1,10 @@
 package env
 
 import (
-	"github.com/joho/godotenv"
 	"os"
 )
 
 func GetApiKey() string {
-	err := LoadEnvModule()
-	if  err != nil {
-		return ""
-	}
-	
 	apiKey := os.Getenv("MAPS_API_KEY")
 	return apiKey
-}
-
-func LoadEnvModule() error {
-	err := godotenv.Load()
-
-	if err != nil {
-		panic("Error loading .env file")
-	}
-
-	return nil
 }
